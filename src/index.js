af5668a31fa2676f236c12bf8063bd168371d11d
yuweichieh <rftheitu0130@gmail.com>
2021-09-19 00:31:41 -0400

    State-base component learned.

--- src/index.js
+++ src/index.js
@@ -5,37 +5,48 @@ import faker from 'faker';
 import CommentDetail from './CommentDetail';
 import ApprovalCard from './ApprovalCard';
 
-// Create React Component
+// (Function Base Component)
+/*
 const App = () => { // () => equals to function()
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err)
+    );
     return (
-        <div className = "ui container comments">
-            <ApprovalCard>
-                <CommentDetail 
-                    author = "Sam"
-                    timeAgo = "Today at 4:21PM"
-                    comment = "Haha"
-                    image = {faker.image.avatar()} 
-                />
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail
-                    author = "Alex"
-                    timeAgo = "Today at 2:00AM"
-                    comment = "Yeah"
-                    image = {faker.image.avatar()}
-                />
-            </ApprovalCard>
-            <ApprovalCard>
-                <CommentDetail
-                    author = "Jane"
-                    timeAgo = "Yesterday at 8:30PM"
-                    comment = "I did it!"
-                    image = {faker.image.avatar()}
-                />
-            </ApprovalCard>
+        <div>
+            <div className = "ui container comments">
+                <ApprovalCard>
+                    <CommentDetail 
+                        author = "Sam"
+                        timeAgo = "Today at 4:21PM"
+                        comment = "Haha"
+                        image = {faker.image.avatar()} 
+                    />
+                </ApprovalCard>
+            </div>
         </div>
     );
 };
+*/
+
+// (Class Base Component)
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { lat: null };
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({ lat: position.coords.latitude });
+            },
+            (err) => console.log(err)
+        );
+    }
+
+    // React requires to define a render function
+    render() {
+        return <div>Latitude: {this.state.lat}</div>
+    }
+}
 
 // Take the React Component and show it on the screen
 ReactDOM.render(
